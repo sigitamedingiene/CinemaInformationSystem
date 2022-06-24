f0b2ddc64351c928f0f4ccc9fe0f67c630fc1b7c
Sigita Medingiene <sigitamedingiene@gmail.com>
2022-06-24 23:14:12 +0300

    Add mainform with buttons. Create Servize folder.

---
+++ CinemaInformationSystemApp/MainForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using CinemaInformationSystemRepository.DBContext;
+using CinemaInformationSystemRepository.Entities;
+
+namespace CinemaInformationSystemApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            CinemaDbContext cinemaDbContext = new CinemaDbContext();
+            Movie movie = new Movie("Cinderella", "Animation", "Walt Disney", DateTime.Today);
+            cinemaDbContext.Add(movie);
+            cinemaDbContext.SaveChanges();
+        }
+
+    }
+}
