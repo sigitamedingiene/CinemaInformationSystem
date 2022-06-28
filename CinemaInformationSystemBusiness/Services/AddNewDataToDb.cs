e59337f7385b94d7efd8c51f0f23e0375628e103
Sigita Medingiene <sigitamedingiene@gmail.com>
2022-06-28 21:24:05 +0300

    Add new service class AddNewDataToDb.

---
+++ CinemaInformationSystemBusiness/Services/AddNewDataToDb.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using CinemaInformationSystemRepository.DBContext;
+using CinemaInformationSystemRepository.Entities;
+
+namespace CinemaInformationSystemBusiness.Services
+{
+    public class AddNewDataToDb
+    {   public Movie _movie;
+        public Auditorium _auditorium;
+        public AddNewDataToDb(Movie movie, Auditorium auditorium)
+        {
+            _movie = movie;
+            _auditorium = auditorium;
+        }
+        CinemaDbContext cinemaDbContext = new CinemaDbContext();
+        public void AddNewAuditorium(int number, string owner, string city, string adress, int placeCount, int rowsCount, int rowSeatCount, Guid movieId)
+        {
+            Auditorium newAuditorium = new(number, owner, city, adress, placeCount, rowsCount, rowSeatCount);
+            movieId = _movie.Id;
+            cinemaDbContext.Auditoriums.Add(newAuditorium);
+            cinemaDbContext.Add(movieId);
+            cinemaDbContext.SaveChanges();
+        }
+        public void AddNewClient(string name, string surName, decimal age, string email, Guid auditoriumId)
+        {
+            Client newClient = new(name, surName, age, email);
+            auditoriumId = _auditorium.Id;
+            cinemaDbContext.Clients.Add(newClient);
+            cinemaDbContext.Add(auditoriumId);
+            cinemaDbContext.SaveChanges();
+        }
+    }
+}
